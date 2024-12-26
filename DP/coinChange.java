b8a4ab82305f41452c68066bbd1f28678a7c8842
Avikam <biravikam@gmail.com>
2024-12-26 06:42:15 +0530

    final

--- DP/coinChange.java
+++ DP/coinChange.java
@@ -1,5 +1,7 @@
 package DP;
 
+import java.util.Arrays;
+
 public class coinChange {
     public static void main(String[] args) {
 
@@ -12,19 +14,22 @@ public class coinChange {
 
     static int coin_Change(int[] coins, int amount) {
         int[] dp = new int[amount + 1];
+
+        // Initialize dp array with a high value (amount + 1 is used as infinity)
+        Arrays.fill(dp, amount + 1);
+
+        // Base case: 0 amount requires 0 coins
         dp[0] = 0;
+
         for (int i = 1; i <= amount; i++) {
-            dp[i] = Integer.MAX_VALUE;
-            
             for (int coin : coins) {
-                if (coin <= i && dp[i - coin] != Integer.MAX_VALUE) {
+                if (i >= coin) {
                     dp[i] = Math.min(dp[i], dp[i - coin] + 1);
                 }
             }
         }
-        if (dp[amount] == Integer.MAX_VALUE) {
-            return -1;
-        }
-        return dp[amount];
+
+        // If dp[amount] is still amount + 1, it means we cannot form the amount
+        return dp[amount] > amount ? -1 : dp[amount];
     }
 }
